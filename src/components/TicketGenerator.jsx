28bfb42b2b7be5fb77bc463c1b17bb6500d5cef5
L0rD-Op <office.snj.2005@gmail.com>
2025-04-13 22:34:23 +0530

    .

--- src/components/TicketGenerator.jsx
+++ src/components/TicketGenerator.jsx
@@ -19,7 +19,7 @@ const TicketGenerator = () => {
         e.preventDefault();
         
         try {
-            const response = await fetch('http://localhost:3000/ticket/external/add', {
+            const response = await fetch('https://143d-2405-201-8018-e032-ccd7-3711-a21f-d6db.ngrok-free.app/ticket/external/add', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
